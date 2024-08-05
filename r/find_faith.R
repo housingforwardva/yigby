6caf541864cfd49ccb3808fc5be56a2174b3851e
ericvmai <30978780+ericvmai@users.noreply.github.com>
2024-08-05 15:45:56 -0400

    Update .R and add homeland data

--- r/find_faith.R
+++ r/find_faith.R
@@ -1,5 +1,6 @@
 library(arrow)
 library(tidyverse)
+library(fuzzyjoin)
 
 va <- open_csv_dataset("data/va_statewide.csv")
 
@@ -23,12 +24,40 @@ write_parquet(va, "data/va_statewide.parquet")
 # The way I like to work with arrow is to treat the dataset as if it were a 
 # database using dplyr.  You can query the larger file, then "collect"
 # the information you need into your R session.  
-#
+
+hifld_worship <- read_csv("data/va_worship.csv") |> 
+  janitor::clean_names() |> 
+  mutate(name = str_to_upper(name))  # Convert to uppercase for consistency
+
+
+faith_parcels <- open_dataset("data/va_statewide.parquet") %>%
+  select(1:2, 11:16, 19:66) |> 
+  mutate(owner_upper = str_to_upper(owner)) %>%
+  collect()
+
+matched_parcels <- faith_parcels %>%
+  fuzzy_left_join(
+    hifld_worship,
+    by = c("owner_upper" = "name"),
+    match_fun = str_detect
+  )
+
+
+
 # Let's try this for the baptist example.
 faith_based_orgs <- open_dataset("data/va_statewide.parquet") |> 
   filter(str_detect(owner, regex("baptist|methodist|episcopal|presbyterian|lutheran|catholic|church|temple|synagogue|mosque|congregation|ministry|chapel|christian|evangelical|pentecostal|adventist|assembly of god|jehovah|latter-day saints|mormon|mennonite|quaker|friends meeting|unitarian|orthodox", ignore_case = TRUE))) |> 
   collect() |> 
   select(1:2, 11:16, 19:66)
+
+faith_based_orgs <- open_dataset("data/va_statewide.parquet") |> 
+  select(1:2, 11:16, 19:66) |> 
+  mutate(
+    is_faith_based = str_detect(owner, regex("baptist|methodist|episcopal|presbyterian|lutheran|catholic|church|temple|synagogue|mosque|congregation|ministry|chapel|(^|\\s)christian|evangelical|pentecostal|adventist|assembly of god|jehovah|latter-day saints|lds|mormon|mennonite|quaker|friends meeting|unitari(an|um)|orthodox", ignore_case = TRUE)),
+    potential_false_negative = str_detect(owner, regex("\\b(christian|christ)(sen|son|ensen|enson)\\b", ignore_case = TRUE))
+  ) |> 
+  filter(is_faith_based | potential_false_negative) |>
+  collect()
 
 # We've cut the original dataset with 4.2 million rows down to 11,541 rows
 # with "(Bb)aptist" in their `owner` column. 
