d4a0d8b45fc051fa484f611d5459076916e4f950
ericvmai <ericvmai@gmail.com>
2024-11-26 15:22:21 -0500

    Correct GA Districts

--- r/faith_assembly.R
+++ r/faith_assembly.R
@@ -5,14 +5,14 @@ library(scales)
 library(rmapshaper)
 
 
-# hdistricts <- geojson_sf("data/faith_gen_assembly.geojson") |> 
-#   st_drop_geometry() |> 
-#   rename(house = NAMELSAD)|> 
-#   rename(senate = NAMELSAD_2) |> 
+# hdistricts <- geojson_sf("data/geo/faith_join_ga_21.geojson") |>
+#   st_drop_geometry() |>
+#   rename(house = hd)|>
+#   rename(senate = sd) |>
 #   distinct(ll_uuid, .keep_all = TRUE)
 # 
-# faith_summary_ga <- hdistricts |> 
-#   group_by(house) |> 
+# faith_summary_ga <- hdistricts |>
+#   group_by(house) |>
 #   summarise(total_acres = as.numeric(format(sum(acres), scientific = FALSE)),
 #             avg_parcel_size = mean(acres),
 #             med_parcel_size = median(acres),
@@ -21,14 +21,14 @@ library(rmapshaper)
 # write_csv(faith_summary_ga, "data/faith_house.csv")
 # 
 # 
-# sdistricts <- geojson_sf("data/faith_gen_assembly.geojson") |> 
-#   st_drop_geometry() |> 
-#   rename(house = NAMELSAD)|> 
-#   rename(senate = NAMELSAD_2) |> 
+# sdistricts <- geojson_sf("data/geo/faith_join_ga_21.geojson") |>
+#   st_drop_geometry() |>
+#   rename(house = hd)|>
+#   rename(senate = sd) |>
 #   distinct(ll_uuid, .keep_all = TRUE)
 # 
-# faith_summary_ga <- sdistricts |> 
-#   group_by(senate) |> 
+# faith_summary_ga <- sdistricts |>
+#   group_by(senate) |>
 #   summarise(total_acres = as.numeric(format(sum(acres), scientific = FALSE)),
 #             avg_parcel_size = mean(acres),
 #             med_parcel_size = median(acres),
@@ -38,50 +38,31 @@ library(rmapshaper)
 
 
 
-hd <- geojson_sf("data/geo/faith_house_clip.geojson") |> 
+hd <- geojson_sf("data/geo/faith_house_21.geojson") |>
   ms_simplify(keep = 0.1, keep_shapes = TRUE) |> 
-  mutate(across(3:5, .fns = ~as.numeric(.x))) |> 
+  mutate(across(4:7, .fns = ~as.numeric(.x))) |> 
   mutate(label_count = comma(faith_house_count)) |> 
-  mutate(NAMELSAD = str_remove(NAMELSAD, "State House District ")) |> 
+  mutate(district = DISTRICT) |> 
   mutate(faith_house_total_acres = round(faith_house_total_acres, digits = 2)) |> 
-  mutate(label_acres = paste(comma((faith_house_total_acres)), "acres", sep = " "))
-
-med_size <- read_csv("data/faith_house.csv") |> 
-  mutate(house = str_remove(house, "State House District ")) |> 
-  select(NAMELSAD = house, med_parcel_size)
-
-hd <- hd |> 
-  left_join(
-    med_size, 
-    by = "NAMELSAD"
-  ) |> 
+  mutate(label_acres = paste(comma((faith_house_total_acres)), "acres", sep = " ")) |> 
   mutate(
-    med_parcel_size = round(med_parcel_size, 2)
+    med_parcel_size = round(faith_house_med_parcel_size, 2)
   ) |> 
   mutate(label_size = round(med_parcel_size, digits = 2))
+
 
 write_rds(hd, "data/rds/faith_house.rds")
 
 
-sd <- geojson_sf("data/geo/faith_senate_clip.geojson") |> 
+sd <- geojson_sf("data/geo/faith_senate_21.geojson") |>
   ms_simplify(keep = 0.1, keep_shapes = TRUE) |> 
-  mutate(across(3:5, .fns = ~as.numeric(.x))) |> 
+  mutate(across(3:7, .fns = ~as.numeric(.x))) |> 
   mutate(label_count = comma(faith_senate_count)) |> 
-  mutate(NAMELSAD = str_remove(NAMELSAD, "State Senate District ")) |> 
+  mutate(district = DISTRICT) |> 
   mutate(faith_senate_total_acres = round(faith_senate_total_acres, digits = 2)) |> 
-  mutate(label_acres = paste(comma((faith_senate_total_acres)), "acres", sep = " "))
-
-med_size <- read_csv("data/faith_senate.csv") |> 
-  mutate(senate = str_remove(senate, "State Senate District ")) |> 
-  select(NAMELSAD = senate, med_parcel_size)
-
-sd <- sd |> 
-  left_join(
-    med_size, 
-    by = "NAMELSAD"
-  ) |> 
+  mutate(label_acres = paste(comma((faith_senate_total_acres)), "acres", sep = " ")) |> 
   mutate(
-    med_parcel_size = round(med_parcel_size, 2)
+    med_parcel_size = round(faith_senate_med_parcel_size, 2)
   ) |> 
   mutate(label_size = round(med_parcel_size, digits = 2))
 
